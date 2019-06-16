a37ba2c2e325bdfe9e758907af874dcf009ab6b7
Xiaohui Liu <jayliu925@gmail.com>
2019-06-16 17:06:41 -0600

    Updates

---
+++ precache-manifest.94117ab388829163eb658966bf1ab0b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a5d7d0273c506722d5fbc290c63a5e",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "81ed645c8b1bea1048de",
+    "url": "/react-calculator/static/css/main.0d2b0a5a.chunk.css"
+  },
+  {
+    "revision": "e20b4d185846297f7567",
+    "url": "/react-calculator/static/js/2.47ce3f0d.chunk.js"
+  },
+  {
+    "revision": "81ed645c8b1bea1048de",
+    "url": "/react-calculator/static/js/main.8dcb33a6.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ergouzii/calculator/precache-manifest.3cdfe9c375a35edf527f339ba16f4b76.js"
+  "/react-calculator/precache-manifest.94117ab388829163eb658966bf1ab0b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ergouzii/calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
